2900d27aa2be0cb2d1e104347f10420675babfdb
ranajitchandra <ranajitchandradas3@gmail.com>
2025-07-21 07:53:57 +0600

    add root link

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000
 // middleware
 app.use(
     cors({
-        origin: ['http://localhost:5173'], // where come  from data, set fronend root use
+        origin: ['http://localhost:5173', 'https://athletic-aim.web.app'], // where come  from data, set fronend root use
         credentials: true, // allow cookie from fronend side
     }),
 )
@@ -92,7 +92,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const eventsCollecttion = client.db('athleticAimDB').collection('events')
         const eventBooking = client.db('athleticAimDB').collection('eventBooking')
@@ -237,8 +237,8 @@ async function run() {
         )
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
